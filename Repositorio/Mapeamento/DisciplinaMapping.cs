857fba6fabc615216bd32be61ef194a03bfe7927
felipearantes <felipe.arantes05@gmail.com>
2020-10-11 15:37:29 -0300

    Implementação do metodo DisciplinaMappping.

--- Repositorio/Mapeamento/DisciplinaMapping.cs
+++ Repositorio/Mapeamento/DisciplinaMapping.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repositorio.Mapeamento
 {
-    public class DisciplinaMapping
+    public class DisciplinaMapping : IEntityTypeConfiguration<Disciplina>
     {
-
+        public void Configure(EntityTypeBuilder<Disciplina> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
