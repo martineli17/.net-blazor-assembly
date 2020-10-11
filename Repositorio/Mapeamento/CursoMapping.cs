97ac05bf5ae3b72ec6042fd09d0d96f1dcc9c2e2
Fabio Martineli <fabio17lmg@outlook.com>
2020-10-11 15:52:07 -0300

    Classes repositórios

--- Repositorio/Mapeamento/CursoMapping.cs
+++ Repositorio/Mapeamento/CursoMapping.cs
@@ -3,8 +3,6 @@ using Dominio.ValuesType;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Repositorio.Mapeamento
 {
---
+++ Repositorio/Repositorios/DisciplinaRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Repositorios
+{
+    public class DisciplinaRepositorio
+    {
+    }
+}
