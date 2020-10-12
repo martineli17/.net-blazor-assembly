d93846d0db36e8e99587ea694d89ac24350e2883
Fabio Martineli <fabio17lmg@outlook.com>
2020-10-12 09:31:58 -0300

    Implementação do UOW e Notificador

--- Repositorio/UnitOfWork/UnitOfWork.cs
+++ Repositorio/UnitOfWork/UnitOfWork.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Crosscuting.Notificacao;
+using Dominio.Interfaces.Repositorio;
+using Repositorio.Contexto;
+using System;
+using System.Threading.Tasks;
 
 namespace Repositorio.UnitOfWork
 {
-    class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly Context _context;
+        private readonly INotificador _notificador;
+        public UnitOfWork(Context context, INotificador notificador)
+        {
+            _context = context;
+            _notificador = notificador;
+        }
+        public async Task<bool> CommitAsync()
+        {
+            try
+            {
+                using (var transacao = await _context.Database.BeginTransactionAsync())
+                {
+                    await _context.SaveChangesAsync();
+                    await transacao.CommitAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _notificador.Add("Ocorreu um erro ao processar a operação.", EnumTipoMensagem.Erro);
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
     }
 }
