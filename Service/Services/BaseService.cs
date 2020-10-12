7f60b2c8e055190a6e06bfd579d5649116bf6bf6
Fabio Martineli <fabio17lmg@outlook.com>
2020-10-12 09:48:47 -0300

    Inicio do serviceBase e Validator

---
+++ Service/Services/BaseService.cs
@@ -0,0 +1,35 @@
+﻿using Dominio.Entidades;
+using Dominio.Interfaces.Service;
+using System;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class BaseService<TEntidade> : IBaseService<TEntidade> where TEntidade : Base
+    {
+        public Task<bool> AddAynsc(TEntidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetAsync(Func<TEntidade, bool> query = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(TEntidade entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Service/Validators/Validator.cs
@@ -0,0 +1,27 @@
+﻿using Crosscuting.Notificacao;
+using FluentValidation;
+using System.Linq;
+
+namespace Service.Validators
+{
+    public class Validator
+    {
+        private readonly INotificador _notificador;
+        public Validator(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+        public bool Executar<TValidator, TObject>(TValidator validator, TObject objeto) where TValidator : AbstractValidator<TObject>
+        {
+            var validacao = validator.Validate(objeto);
+            if (!validacao.IsValid)
+                _notificador.AddRange(validacao.Errors.Select(x => x.ErrorMessage).ToList(), EnumTipoMensagem.Warning);
+            return validacao.IsValid;
+        }
+    }
+
+    public interface IValidator 
+    {
+        bool Executar<TValidator, TObject>(TValidator validator, TObject objeto) where TValidator : AbstractValidator<TObject>;
+    }
+}
