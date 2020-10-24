422245c7921b46332ffec94def0edcd2018ec17a
Fabio Martineli <fabio17lmg@outlook.com>
2020-10-24 11:20:10 -0300

    Service do Aluno.

--- Dominio/Interfaces/Service/IAlunoService.cs
+++ Dominio/Interfaces/Service/IAlunoService.cs
@@ -1,8 +1,11 @@
 ﻿using Dominio.Entidades;
+using System.Threading.Tasks;
 
 namespace Dominio.Interfaces.Service
 {
     public interface IAlunoService : IBaseService<Aluno>
     {
+        Task<Aluno> AddAsync(Aluno entidade);
+        Task<Aluno> UpdateAsync(Aluno entidade);
     }
 }
